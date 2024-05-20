047edc900f3340c24300fa2e951aba60094309eb
Mordechai (Moti) Botrashvily <mbotrashvily@acm.org>
2024-05-20 18:04:29 +0300

    - More description of MOSEFT pin connection and channel numbering.

--- peltier_pixels/peltier_pixels.ino
+++ peltier_pixels/peltier_pixels.ino
@@ -3,11 +3,19 @@
  * The user can configure each channel through serial commands, either by 
  * direct USB or via a Bluetooth connection.
  *
+ * MOSFET channel to Arduino pin mapping (using proto shield extension):
+ * - CH0 -> 8
+ * - CH1 -> 9
+ * - CH2 -> 10
+ * - CH3 -> 11
+ *
  * Supported commands:
  * - ch<X>const - Set channel <X> to constant value
  * - ch<X>sine<hz> - Set channel <X> to sine wave with <hz> frequency (can be float)
  * - ch<X>amp<value> - Set channel <X> output amplitude to <value>
  * - ch<X>mod<value> - Set channel <X> output modulation (time shift) to <value> (can be float)
+ *
+ * <X> can be 0, 1, 2 or 3. If set to 9 than all channels are modified.
  * 
  * Author: Mordechai Botrashvily
  * Date: 20/05/2024
