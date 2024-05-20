7f73ccca7f62857f5e45af54d6dd9883e7c5dc1a
Mordechai (Moti) Botrashvily <mbotrashvily@acm.org>
2024-05-20 17:58:47 +0300

    - Added skeleton sketch with initial description.

---
+++ peltier_pixels/peltier_pixels.ino
@@ -0,0 +1,24 @@
+/*
+ * This sketch operates multiple peltier elements using a multi-channel MOSFET.
+ * The user can configure each channel through serial commands, either by 
+ * direct USB or via a Bluetooth connection.
+ *
+ * Supported commands:
+ * - ch<X>const - Set channel <X> to constant value
+ * - ch<X>sine<hz> - Set channel <X> to sine wave with <hz> frequency (can be float)
+ * - ch<X>amp<value> - Set channel <X> output amplitude to <value>
+ * - ch<X>mod<value> - Set channel <X> output modulation (time shift) to <value> (can be float)
+ * 
+ * Author: Mordechai Botrashvily
+ * Date: 20/05/2024
+ */
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
