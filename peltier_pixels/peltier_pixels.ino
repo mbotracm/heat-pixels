9f1707d85eb2cbf7ed3de118be6c1b131e741e30
Mordechai (Moti) Botrashvily <mbotrashvily@acm.org>
2024-05-24 17:05:00 +0300

    - Shortened subcommands to allow easy and consistent parsing. - Added constant values for subcommands. - Added placeholder for handling subcommands.

--- peltier_pixels/peltier_pixels.ino
+++ peltier_pixels/peltier_pixels.ino
@@ -10,8 +10,8 @@
  * - CH3 -> 11
  *
  * Supported commands:
- * - ch<X>const - Set channel <X> to constant value
- * - ch<X>sine<hz> - Set channel <X> to sine wave with <hz> frequency (can be float)
+ * - ch<X>con - Set channel <X> to constant value
+ * - ch<X>sin<hz> - Set channel <X> to sine wave with <hz> frequency (can be float)
  * - ch<X>amp<value> - Set channel <X> output amplitude to <value>
  * - ch<X>mod<value> - Set channel <X> output modulation (time shift) to <value> (can be float)
  *
@@ -22,16 +22,20 @@
  */
 
 String COMMAND_PREFIX("ch");
+String SUBCOMMAND_PREFIX_CONST("con");
+String SUBCOMMAND_PREFIX_SINE("sin");
+String SUBCOMMAND_PREFIX_AMPLITUDE("amp");
+String SUBCOMMAND_PREFIX_MODULATION("mod");
 
 void setup() {
   // Configure serial channel to baudrate of 115,200 bps.
   Serial.begin(115200);
-
 }
 
 void loop() {
   // Process input commands, should always start with ch and a digit.
   char commandPrefix[2];
+  char subcommandPrefix[4] = {0};
   int channel = -1;
   if (Serial.available() > 2) {
     Serial.readBytes(commandPrefix, 2);
@@ -40,15 +44,37 @@ void loop() {
     if (COMMAND_PREFIX == commandPrefix) {
       channel = Serial.parseInt();
       Serial.print("Received command for channel: ");
-      switch (channel) {
-        case 0:
-          Serial.println("0");
-          break;
-        default:
-          Serial.println("unknown...");
-          break;
+      Serial.println(channel);
+
+      // Parse specific command.
+      Serial.readBytes(subcommandPrefix, 3);
+      Serial.print("Subcommand: ");
+      Serial.println(subcommandPrefix);
+
+      // Handle subcommands.
+      // Constant output value.
+      if (SUBCOMMAND_PREFIX_CONST == subcommandPrefix)
+      {
+        /* code */
       }
-    } else {
+      // Sine output value.
+      if (SUBCOMMAND_PREFIX_SINE == subcommandPrefix)
+      {
+        /* code */
+      }
+      // Set output amplitude.
+      if (SUBCOMMAND_PREFIX_AMPLITUDE == subcommandPrefix)
+      {
+        /* code */
+      }
+      // Set output sine wave modulation.
+      if (SUBCOMMAND_PREFIX_MODULATION == subcommandPrefix)
+      {
+        /* code */
+      }
+    }
+    else
+    {
       Serial.print("Unrecognized command: ");
       Serial.print(commandPrefix);
       Serial.println(Serial.readStringUntil('\n'));
